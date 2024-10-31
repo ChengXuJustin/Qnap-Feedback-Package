6ec5d55645c9a3592257f02584fcd33651815e91
ChengXu Pan <chengxupan@ChengXudeMacBook-Pro.local>
2024-10-31 23:50:58 +0800

    feedback update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
-import Feedback from './components/Feedback'
+// import Feedback from './components/Feedback'
+import Citation from './components/Citation'
+import Feedback_temp from './components/Feedback_temp'
+import rating_text from './data/rating.json'
+
 
 function App() {
 
   return (
     <>
-      <Feedback />
+      {/* <Feedback /> */}
+      <Citation />
+
+      <Feedback_temp 
+        id="rating"
+        data={rating_text}
+      />
+
+      <Feedback_temp 
+        id="openQues"
+        data={{}}
+      />
     </>
   )
 }
